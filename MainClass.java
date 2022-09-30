4097006995240ec2a5871a4f2820614c6351d13b
Learn with fun <80329368+dearam@users.noreply.github.com>
2022-09-30 17:00:00 +0530

    Update MainClass.java
    Here the updation

--- MainClass.java
+++ MainClass.java
@@ -9,24 +9,35 @@ public class MainClass {
 		try {
 			Connection con=DriverManager.getConnection(url,name,password);
 			Statement st=con.createStatement();
-//			String create="create table bey"+"(id int,"+"name varchar(20))";
-//			st.executeUpdate(create);
-//			System.out.println("created");
-//			PreparedStatement ps=con.prepareStatement("insert into bae values(?,?,?)");
-//			ps.setInt(1,1);
-//			ps.setString(2,"dearam");
-//			ps.setInt(3, 20);
-//			int c=ps.executeUpdate();
+			//for creating table
+			String create="create table bey"+"(id int,"+"name varchar(20))";
+			st.executeUpdate(create);
+			System.out.println("created");
+			
+			//for inserting values
+			PreparedStatement ps=con.prepareStatement("insert into bae values(?,?,?)");
+			ps.setInt(1,1);
+			ps.setString(2,"dearam");
+			ps.setInt(3, 20);
+			int c=ps.executeUpdate();
 			System.out.println("added");
-//			int dlt=st.executeUpdate("delete from bey where id=2");
-//			System.out.println("deleted");
-			//int result=st.executeUpdate("update hello set name='elon musk' where id=5");
+			
+			//for deleting row from table
+			int dlt=st.executeUpdate("delete from bey where id=2");
+			System.out.println("deleted");
+			
+			//for updating the value
+			int result=st.executeUpdate("update hello set name='elon musk' where id=5");
+			
+			//for printing all those element
 			ResultSet rs=st.executeQuery("select * from bae order by age asc");
 			while(rs.next()) {
 				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
 			}
+			
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+
 	}
 	}
